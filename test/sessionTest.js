a33d59dc20a8feadf73bf96b90c3b8a68645e6dc
Rob De Feo <robertodefeo@hotmail.com>
2014-01-13 16:40:30 +0800

    fixed a bunch of stupid problems involving not using the unique list carefully generated in one of the steps

--- test/sessionTest.js
+++ test/sessionTest.js
@@ -211,11 +211,11 @@ describe('session', function(){
       
       var target = session.groupedMinMax(items); 
       assert.equal(target.max, 1389336292509);
-      assert.equal(target.min, 1389336072509);
+      assert.equal(target.min, 1389336137509);
       assert.equal(target.colour.max, 1389336292509);
       assert.equal(target.colour.min, 1389336137509);
       assert.equal(target.style.max, 1389336219509);
-      assert.equal(target.style.min, 1389336072509);
+      assert.equal(target.style.min, 1389336219509);
     });    
   });
   describe('processItems', function(){
@@ -254,10 +254,10 @@ describe('session', function(){
       var target = session.processItems(context); 
       assert.equal(target.length, 2);
       assert.equal(target[0].ageScore, 2);
-      assert.equal(target[0].type, "colour");
+      assert.equal(target[0].type, "style");
       
       assert.equal(target[1].ageScore, 2);
-      assert.equal(target[1].type, "style");
+      assert.equal(target[1].type, "colour");
       
     });
     
@@ -287,23 +287,28 @@ describe('session', function(){
             uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
             timestamp: '2014-01-10T06:04:53.509Z',
             data: { type: 'colour', value: 'yellow', method: 'exact' },
+            _id: '52cf96e4e9d40c0000d64696' },
+        { sessionID: '6c3c9f61-8f22-4e4c-8619-2eb0282b14df',
+            uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
+            timestamp: '2014-01-10T06:26:13.509Z',
+            data: { type: 'colour', value: 'yellow', method: 'exact' },
             _id: '52cf96e4e9d40c0000d64696' }
         
       ];
       
       var target = session.processItems(context); 
-      console.dir(target);
+
       assert.equal(target.length, 5);
       assert.equal(target[0].ageScore, 2);
-      assert.equal(target[0].type, "colour");
+      assert.equal(target[0].type, "style");
       
       assert.equal(target[1].ageScore, 2);
-      assert.equal(target[1].type, "style");
+      assert.equal(target[1].type, "colour");
 
       assert.equal(target[2].ageScore, 1);
       assert.equal(target[2].type, "style");
 
-      assert.equal(target[3].ageScore, 1.7503126302626093);
+      assert.equal(target[3].ageScore, 1.4647006255585344);
       assert.equal(target[3].type, "colour");
 
       assert.equal(target[4].ageScore, 1);
@@ -311,66 +316,4 @@ describe('session', function(){
       
     });
   })
-})
-  
-describe('session', function(){
-  describe('processItems', function(){
-    it('should return -1 when the value is not present', function(){
-      var context = [ { sessionID: '6c3c9f61-8f22-4e4c-8619-2eb0282b14df',
-          uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
-          timestamp: '2014-01-10T06:44:52.509Z',
-          data: { type: 'colour', value: 'pink', method: 'exact' },
-          _id: '52cf96e4e9d40c0000d64696' },
-        { sessionID: '6c3c9f61-8f22-4e4c-8619-2eb0282b14df',
-          uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
-          timestamp: '2014-01-10T06:44:52.509Z',
-          data: { type: 'colour', value: 'blue', method: 'exact' },
-          _id: '52cf96e4e9d40c0000d64697' },
-        { sessionID: '6c3c9f61-8f22-4e4c-8619-2eb0282b14df',
-          uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
-          timestamp: '2014-01-10T06:44:52.509Z',
-          data: { type: 'style', value: 'pumps', method: 'exact' },
-          _id: '52cf96e4e9d40c0000d64698' },
-        { sessionID: '6c3c9f61-8f22-4e4c-8619-2eb0282b14df',
-          uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
-          timestamp: '2014-01-10T06:46:22.317Z',
-          data: { type: 'colour', value: 'black', method: 'exact' },
-          _id: '52cf973ee9d40c0000d64699' },
-        { sessionID: '6c3c9f61-8f22-4e4c-8619-2eb0282b14df',
-          uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
-          timestamp: '2014-01-10T06:46:22.317Z',
-          data: { type: 'colour', value: 'blue', method: 'exact' },
-          _id: '52cf973ee9d40c0000d6469a' },
-        { sessionID: '6c3c9f61-8f22-4e4c-8619-2eb0282b14df',
-          uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
-          timestamp: '2014-01-10T06:46:22.317Z',
-          data: { type: 'colour', value: 'green', method: 'exact' },
-          _id: '52cf973ee9d40c0000d6469b' },
-        { sessionID: '6c3c9f61-8f22-4e4c-8619-2eb0282b14df',
-          uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
-          timestamp: '2014-01-10T07:26:05.798Z',
-          data: { type: 'colour', value: 'blue', method: 'exact' },
-          _id: '52cfa08de9d40c0000d6469c' },
-        { sessionID: '6c3c9f61-8f22-4e4c-8619-2eb0282b14df',
-          uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
-          timestamp: '2014-01-10T07:33:45.569Z',
-          data: { type: 'colour', value: 'green', method: 'exact' },
-          _id: '52cfa259e9d40c0000d6469d' },
-        { sessionID: '6c3c9f61-8f22-4e4c-8619-2eb0282b14df',
-          uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
-          timestamp: '2014-01-10T07:34:27.092Z',
-          data: { type: 'colour', value: 'blue', method: 'exact' },
-          _id: '52cfa283e9d40c0000d6469e' },
-        { sessionID: '6c3c9f61-8f22-4e4c-8619-2eb0282b14df',
-          uuid: 'f46b177e-4add-4a27-993c-de69f2ef9b2d',
-          timestamp: '2014-01-10T07:34:48.479Z',
-          data: { type: 'colour', value: 'yellow', method: 'exact' },
-          _id: '52cfa298e9d40c0000d6469f' } ];
-          
-          
-          // processItems
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+})  
