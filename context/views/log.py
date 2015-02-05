999d79f3406bcdf99468181bf96ecf4a8c5b8baf
Rob De Feo <robertodefeo@hotmail.com>
2015-02-05 13:06:53 +0100

    closes #13 add timestamp to the log

--- context/views/log.py
+++ context/views/log.py
@@ -3,6 +3,8 @@ __author__ = 'robdefeo'
 from flask import Blueprint, jsonify, request
 import logging
 from context.data.log import Log
+from datetime import datetime
+
 mod_log = Blueprint('log', __name__, url_prefix='/logs/')
 LOGGER = logging.getLogger(__name__)
 
@@ -16,6 +18,8 @@ def test(_type):
 @mod_log.route('<_type>', methods=['POST'])
 def log(_type):
     j = request.get_json(force=True)
+    date = datetime.now().isoformat()
+    j["timestamp"] = date
     data_access_log.insert(_type, j)
 
     return jsonify({})
