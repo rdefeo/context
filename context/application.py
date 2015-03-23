519931e3f40412fc32409469b5dfc9b1f326bbd1
Rob De Feo <robertodefeo@hotmail.com>
2015-03-23 13:16:32 +0100

    add status page closes #22

---
+++ context/application.py
@@ -0,0 +1,21 @@
+import tornado
+from tornado.web import url
+# from context.handlers.root import Root
+from context.handlers.status import Status
+
+__author__ = 'robdefeo'
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            # url(r"/", Root, dict(content=Content()), name="root"),
+            url(r"/status", Status, name="status")
+        ]
+
+        settings = dict(
+            # static_path = os.path.join(os.path.dirname(__file__), "static"),
+            # template_path = os.path.join(os.path.dirname(__file__), "templates"),
+            debug=tornado.options.options.debug,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
