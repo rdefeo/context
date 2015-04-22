739049e8b97294e149357ca27593eb218630394a
Rob De Feo <robertodefeo@hotmail.com>
2015-04-22 18:04:30 +0200

    switch to feedback

--- context/data/feedback.py
+++ context/data/feedback.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import
 from datetime import datetime
 import logging
+from bson.code import Code
+from bson.son import SON
+from datetime import datetime
+from datetime import datetime, timedelta
 from pkg_resources import VERSION
 import context
 from context.data.base import Base
@@ -32,3 +36,69 @@ class Feedback(Base):
             record["meta_data"] = meta_data
 
         self.collection.insert(record)
+
+    def map_product_feedback(self, now=datetime.now(), days_behind=30):
+        mapper = Code("""
+            function(){
+                emit (
+                    {
+                        product_id: this.product_id
+                    },
+                    {
+                        affiliate_redirect_count: (this.type == "affiliate_redirect") ? 1 : 0,
+                        detail_count: (this.type == "detail") ? 1 : 0,
+                        heart_count: (this.type == "heart") ? 1 : 0,
+                        total_count: 1
+                    }
+                )
+            }
+        """)
+        reducer = Code("""
+            function(key, values) {
+                var result = {
+                    affiliate_redirect_count: 0,
+                    detail_count: 0,
+                    heart_count: 0,
+                    total_count: 0
+                };
+                values.forEach(function(value) {
+                    result.affiliate_redirect_count += value.affiliate_redirect_count;
+                    result.detail_count += value.detail_count;
+                    result.heart_count += value.heart_count;
+                    result.total_count += value.total_count;
+                });
+
+                return result;
+
+            }
+        """)
+
+        timestamp = (now - timedelta(days=days_behind)).isoformat()
+
+        self.LOGGER.info("generate=product_interest,updated=%s,out=product_interest,action=replace,db=generate", timestamp)
+
+        result = self.collection.map_reduce(
+            mapper,
+            reducer,
+            query={
+                "type": {
+                    "$in": [
+                        "detail",
+                        "heart",
+                        "affiliate_redirect"
+                    ]
+                },
+                "updated": {
+                    "$gte": timestamp
+                }
+            },
+            out=SON(
+                [
+                    ("replace", "product_interest"),
+                    ("db", "generate")
+                ]
+            )
+
+        )
+
+        return result
